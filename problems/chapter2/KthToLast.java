c694f0015b9e065729489d064215ee126416cdad
Catalin Torge <torgeadelin@gmail.com>
2019-11-23 15:59:55 +0000

    add recursive and runner technique for kth elem

--- problems/chapter2/KthToLast.java
+++ problems/chapter2/KthToLast.java
@@ -25,6 +25,7 @@ class Node {
 }
 
 class KthToLast {
+    static int index = 0;
 
     public static void main(String[] args) {
         Node l = new Node(0);
@@ -34,10 +35,11 @@ class KthToLast {
         l.next.next.next.next = new Node(1);
         l.next.next.next.next.next = new Node(5);
         l.print();
-        System.out.println(findKth(l, Integer.parseInt(args[0])));
+        System.out.println(findKthRunner(l, Integer.parseInt(args[0])));
 
     }
 
+    // This is too simple
     public static int findKth(Node head, int k) {
         Node current = head;
         int size = 0;
@@ -47,6 +49,33 @@ class KthToLast {
         }
         current = head;
         for (int i = 0; i < size - k; ++i) {
+            current = current.next;
+        }
+        return current.value;
+    }
+
+    // Let's assume we can't compute the length
+    // Use recursion
+    public static int findKthRecursive(Node head, int k) {
+        if (head == null) {
+            return 0;
+        }
+        int index = findKthRecursive(head.next, k) + 1;
+        if (k == index) {
+            System.out.println(head.value);
+        }
+
+        return index;
+    }
+
+    // Runner Technique
+    public static int findKthRunner(Node head, int k) {
+        Node fast = head;
+        Node current = head;
+        for (int i = 0; i < k - 1; ++i)
+            fast = fast.next;
+        while (fast.next != null) {
+            fast = fast.next;
             current = current.next;
         }
         return current.value;
